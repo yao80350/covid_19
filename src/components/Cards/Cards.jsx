71555edfbcfde8d2ac60aa229d65d1f7a4f12553
yao80350 <yao80350@gmail.com>
2020-06-13 12:41:28 +0800

    Added Cards Component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,77 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import cx from "classnames";
 
-const Cards = () => {
-    return <div>Cards</div>;
+import styles from "./Cards.module.css";
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if (!confirmed) {
+        return "Loading...";
+    }
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.infected)}
+                >
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Infected
+                        </Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={confirmed.value}
+                                duration={1}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">
+                            {new Date(lastUpdate).toDateString()}
+                        </Typography>
+                        <Typography variant="body2">1</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.recovered)}
+                >
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Recovered
+                        </Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">2</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid
+                    item
+                    component={Card}
+                    xs={12}
+                    md={3}
+                    className={cx(styles.card, styles.deaths)}
+                >
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Deaths
+                        </Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">3</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default Cards;
